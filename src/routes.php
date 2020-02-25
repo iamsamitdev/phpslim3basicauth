3b8a1fbf16b5194c8cb98ba9b6ef7441cd7da559
Samit Koyom <samitkoyom@gmail.com>
2020-02-26 03:11:58 +0700

    fisrt commit

---
+++ src/routes.php
@@ -0,0 +1,26 @@
+<?php
+
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+return function (App $app) {
+
+    $container = $app->getContainer();
+
+    $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
+        // Sample log message
+        $container->get('logger')->info("Slim-Skeleton '/' route");
+
+        // Render index view
+        return $container->get('renderer')->render($response, 'index.phtml', $args);
+    });
+
+
+    $app->group('/api/', function () use ($app) {
+        $app->get('users', function (Request $request, Response $response, array $args) {
+            echo "Hello every body.";
+        });
+    });
+    
+};
